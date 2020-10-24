52be2e0561dc0fdb8e858899403cede7dd4f4027
Mauro <mfconsolani@outlook.com>
2020-10-24 21:13:34 +0200

    added imageUrl to menu-item component and size for images

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,8 +48,8 @@ class Directory extends React.Component {
         render() {
             return (
                 <div className = 'directory-menu'>
-                    {this.state.sections.map(({title, imageUrl, id}) => (
-                            <MenuItem title={title} key={id} imageUrl={imageUrl}/>
+                    {this.state.sections.map(({title, imageUrl, id, size}) => (
+                            <MenuItem title={title} key={id} imageUrl={imageUrl} size={size}/>
                         ))}
                 </div>
             );
