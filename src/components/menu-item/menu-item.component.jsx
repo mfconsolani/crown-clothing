bf35225fd46a53ba49b34a3058937caf82d17bdb
Mauro <mfconsolani@outlook.com>
2020-10-27 14:56:58 +0100

    added style to menu-item component and added a div to menu-item component that will allows us to have an effect on the background image when hovering

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,10 +4,12 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
     <div 
+    className={`${size} menu-item`}>
+    <div className='background-image'
     style={{
         backgroundImage: `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}>
+    }}>
+    </div>
         <div className='content'>
             <h1 className='title'> {title.toUpperCase()} </h1>
             <span className='subtitle'>SHOP NOW!</span>
