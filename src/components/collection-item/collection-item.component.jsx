dd4425dd6ee88fd861df4e8137b34c4aba229234
Mauro <mfconsolani@outlook.com>
2020-11-02 16:14:43 +0100

    updating app with a shop page, collection preview component and collection item component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -8,10 +8,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
         className='image'
         style={{
             backgroundImage: `url(${imageUrl})`
-        }}>
-
+        }}/>
+        <div className='collection-footer'>
+            <span className='name'>{ name }</span>
+            <span className='price'>{ price }</span>
         </div>
     </div>
 )
 
 export default CollectionItem;
+
