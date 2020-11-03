0aa53c4bd646c47b0cadc0af8ecab29c649db451
Mauro <mfconsolani@outlook.com>
2020-11-03 02:02:31 +0100

    added and styled custom-button, form-input, sign-in input components and sign-in-and-sign-up class component, also installed firebase package

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import signInAndSignUpPage from './pages/sing-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={signInAndSignUpPage}/>
       </Switch>
     </div>
   );
