c98fd581f119e3085cc050a8eaf900097bcb95c8
Mauro <mfconsolani@outlook.com>
2020-11-06 15:21:37 +0100

    Began to configure firebase.utils to set up firestore. Began to write the firestore function to detect new and already signed up users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const config = {
     appId: "1:95369486620:web:b7018127aba31dfa165ad9"
   };
 
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+
+      } catch(error) {
+        console.log('error creating user', error.message)
+      }
+    }
+}
+
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
