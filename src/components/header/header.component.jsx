911e882abdc7397e3395b06b7c69638815ac12b0
Mauro <mfconsolani@outlook.com>
2020-11-19 16:14:53 +0100

    added connect store method to Header component as well as mapStateToProps te be able to access the Redux store reducer data. Also, deleted the props passed down to Header component in App.js given that we will no longer need it as we have Header component set up to work with Redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import { ReactComponent as Logo } from '../assets/crown.svg';
 
 import { auth } from '../../firebase/firebase.utils';
@@ -30,5 +32,8 @@ const Header = ({ currentUser }) => (
     </div>
 )
 
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
 
-export default Header;
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
