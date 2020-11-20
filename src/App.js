c8093de7e44b7805254cbce6e7dc81cd9bd61770
Mauro <mfconsolani@outlook.com>
2020-11-20 14:20:06 +0100

    we erased the constructor class from App.js because we don't need that state anymore since that the state is now stored in Redux Store. Also, replaced this.setState for this.props.setCurrentState action, which was also destructured for better looking purposes.

--- src/App.js
+++ src/App.js
@@ -13,34 +13,25 @@ import { setCurrentUser } from './redux/user/user.action';
 
 
 class App extends React.Component {
-  constructor() {
-  super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
+
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
             ...snapShot.data()
-            }
           });
-
-          console.log(this.state);
-
         })
       } else {
-        this.setState({currentUser: userAuth})
+        setCurrentUser(userAuth)
       }
     });
   }
