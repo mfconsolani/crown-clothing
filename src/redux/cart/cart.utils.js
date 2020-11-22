13a392e5f7d00e2ce5f118373a200c6f4aaf00c3
Mauro <mfconsolani@outlook.com>
2020-11-22 15:41:29 +0100

    we added a cart.utils file where we developed a function that will be passed to the cart reducer and allow us to check if the item added to the cart is new or if there is one similar in the cart. if its new, it adds a quantity prop and set it to 1, if its not new, it adds 1 to the quantity prop. also, passed that utils function to the cart reducer so it executes when the action is dispatched

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,24 @@
+// This file will hold any utility functions related to our cart redux code
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    // The find() method searches through an array and gives you 1 item based on your provided test. 
+    // Array find() only returns one item. If you want to get more than one item, look at filter()
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id)
+
+    // find() will return the first item found and store that found item in the existingCartItem constant if
+    // the id of the cart item we are adding matches one in the cart items list; otherwise, it will
+    // return undefined
+
+    if (existingCartItem) {
+        // We need map because we need to return new versions of our state so our components know
+        // that they have to re-render properly
+        return cartItems.map(cartItem => cartItem.id === cartItemToAdd.id 
+            ? { cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+            )
+    } else {
+        // At some point, an item will always be added for the first time; when that happens, quantity property
+        // will be added to the object. 
+        return [...cartItems, {...cartItemToAdd, quantity: 1} ]
+    }
+};
