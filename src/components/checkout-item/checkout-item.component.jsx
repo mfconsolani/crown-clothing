2f70bdbfed1048c85603937f2256e278037d2aaf
Mauro <mfconsolani@outlook.com>
2020-11-23 23:03:46 +0100

    started composing the checkout page where you can see all the items added to the cart and get ready for payment. Also, implemented an additional action for the GoToCheckOut buton in the cart dropdown component that will toggle the hidden property so the dropdown component hides when you click it, which also redirects you to the check out page

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+
+import './checkout-item.styles.scss';
+
+
+const CheckoutItem = ({ cartItem: {name, imageUrl, price, quantity}}) => (
+    <div className='checkout-item'>
+        <div className='image-container'>
+            <img src={imageUrl} alt='item' />
+        </div>
+        <span className='name'>{name}</span>
+        <span className='quantity'>{quantity}</span>
+        <span className='price'>{price}</span>
+        <div className='remove-button'>&#10005;</div>
+    </div>
+)
+
+// const mapDispatchToProps = dispatch =>
+
+// const mapStateToProps = createStructuredSelector({
+
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CheckoutItem);
+
+export default CheckoutItem;
