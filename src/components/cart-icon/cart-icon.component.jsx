4ae82ec2c66943c95bd443228dfa903df4aab33e
Mauro <mfconsolani@outlook.com>
2020-11-23 15:32:31 +0100

    created a selector for itemCount and also reused that selector for cart-dropdown component to avoid performance issues. Also installed reselect package for this purpose

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as ShoppingIcon } from '../assets/shopping-bag.svg';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 
 import './cart-icon.styles.scss';
 
@@ -17,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 }) 
 
-const mapStateToProps = ({ cart: { cartItems }}) => ({
-    itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
-})
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
