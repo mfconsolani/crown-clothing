4dd2ff3082e216cb3e123f97aee05e9e01c277b2
Mauro <mfconsolani@outlook.com>
2020-11-24 01:11:07 +0100

    added decrease and increase functionality to the checkout-item component. For that, created the reducer with its corresponding types, actions and utilities

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -6,9 +6,13 @@ export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
-
 export const addItem  = item => ({
     type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
+
+export const removeItem  = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
     payload: item
 });
 
