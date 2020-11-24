05045b838662d35f59752490e86c766c8bd7b433
Mauro <mfconsolani@outlook.com>
2020-11-24 13:20:10 +0100

    Added react-persistance library and configuration to be able to remember the items in the cart whenever we refresh the page

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist'; //We also want to persist our Rootreducer
+import storage from 'redux-persist/lib/storage' // We import the actual localStorage object on our
+                                                // window browser
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
-    user: userReducer,
+const persistConfig = {
+    key: 'root', // Which represents at what point inside of our reducing object do we want to start storing everything
+    storage, 
+    whitelist: ['cart'] // array containing the string name of the reducers we want to store
+}
+
+const rootReducer = combineReducers({
+    user: userReducer, // This persistance is being handled ny firebase auth at this point
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
+// this will return a modified version of the rootReducer with the persistConfig on top of it
