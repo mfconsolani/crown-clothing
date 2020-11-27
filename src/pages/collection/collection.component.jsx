29b8c6e0890dcdecf783cce69cfc8371a7c22283
Mauro <mfconsolani@outlook.com>
2020-11-27 14:38:44 +0100

    installed loadash memoize module to memoize selectCollection selector which is used to map through the collections category, see if it matches the url parameter and display that specific collection, i.e hats, or jackets, etc. also created the selectCollection selector mentioned before with a currying format that access to the CollectionPage properties to see at what url the application is at and, as said before, display that specific collection

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import './collection.styles.scss';
 
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
     <div className='collection-page'>
         <h2>COLLECTION PAGE</h2>
     </div>
 );
 
-export default CollectionPage;
+// We have to use the 2nd parameter of mapStateToProps which is called ownProps and is the props of 
+// the component that we are wrapping in our connect().
+
+// ownProps.match.params.collectionId -> we have access to match because the Route component that comes 
+// the ShopPage component give us access in this component to the match, history and location property.
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+// selectCollection takes 2 params: the first one is the argument for selectCollection selector, 
+//which if you look in the ShopPage.selectors file you will see that it requires one parameter called 
+//collectionUrlParam; the second one is the state, which is required by every createSelector function, 
+//which also lives inside our selectionCollection, see below:
+// from shop.selects.justify
+
+//     export const selectCollection = collectionUrlParam => 
+//      createSelector(
+//          [selectCollections],
+//          collections => collections.find(
+//              collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+//      )
+
+export default connect(mapStateToProps)(CollectionPage);
