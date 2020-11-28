4e3e8d7be688a4c910c44633e89f889d63a89bbe
Mauro <mfconsolani@outlook.com>
2020-11-29 00:08:59 +0100

    having normalized the shop.data.js, we had to change the selectCollection selector to compare the url.param to the shop.data normalized object

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,25 +1,14 @@
 import { createSelector } from 'reselect';
-import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop;
-
-const COLLECTION_ID_MAP = { 
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-};
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
 
-export const selectCollection = memoize((collectionUrlParam) => 
+export const selectCollection = (collectionUrlParam) => 
     createSelector(
         [selectCollections],
-        collections => collections.find(
-            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-    )
-);
+        collections => collections[collectionUrlParam]
+    );
