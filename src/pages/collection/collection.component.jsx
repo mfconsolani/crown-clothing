6e76fd6a6453a623e98da8e20765b101800a52ee
Mauro <mfconsolani@outlook.com>
2020-11-28 15:18:10 +0100

    normalized data in shop.data.js.Kept building collection-component.js and styled it

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,11 +8,17 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
 
-const CollectionPage = ({ collection }) => (
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return (   
     <div className='collection-page'>
-        <h2>COLLECTION PAGE</h2>
-    </div>
-);
+        <h2 className='title'>{ title }</h2>
+        <div className='items'>
+            {
+                items.map( item => (<CollectionItem key={item.id} item={item}/>))
+            }
+        </div>
+    </div>)}
 
 // We have to use the 2nd parameter of mapStateToProps which is called ownProps and is the props of 
 // the component that we are wrapping in our connect().
