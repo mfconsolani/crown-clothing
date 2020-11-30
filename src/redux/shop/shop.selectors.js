29b602c433ec26c46605a4aafba3795abed5e50e
Mauro <mfconsolani@outlook.com>
2020-11-30 16:35:47 +0100

    As we have the shop.data as an object, afeter being normalized, we also need that data to be mapped in the collection-overview component so for that we need to have a selector that takes the collection, extract the keys and values from the shop.data object and turns it into a 'mapable' object to be used in the collections-overview component; and thats what we did here!

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,18 @@ export const selectCollections = createSelector(
     shop => shop.collections
 );
 
+// As the shop.data is now an object, and in CollectionsOverview component we map through it,
+// now we have to "convert it" to an array so the map function still works
+
+export const selectCollectionForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+    // Here we take the keys from the collections and create an array from them.
+    //Then we use the elements in that array as keys for the items inside the collections, 
+    // to create a new array that allows us to hold the items belonging to each
+    // collection in a new array. BASICALLY WE CONVERT AN OBJECT TO AN ARRAY
+)
+
 export const selectCollection = (collectionUrlParam) => 
     createSelector(
         [selectCollections],
